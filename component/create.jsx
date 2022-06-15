ec60d531f39d7f89a5d13546591f4f56bd07cef7
phlourenco735 <61694156+phlourenco735@users.noreply.github.com>
2022-06-15 10:51:41 -0300

    Add files via upload

--- component/create.jsx
+++ component/create.jsx
@@ -27,6 +27,7 @@ export default function Create() {
         setEmail('')
         setTelefone('')
         setMensagem('')
+        window.location.reload()
       })
   }
  
--- component/read.jsx
+++ component/read.jsx
@@ -3,7 +3,7 @@ import { useState,useEffect} from 'react'
 
 //importar a config do firebase
 import { app, database } from '../services/firebase'
-import { collection,addDoc, getDocs } from 'firebase/firestore';
+import { collection,addDoc, getDocs, orderBy, query, doc, deleteDoc} from 'firebase/firestore';
 
 //configurar o Firebase do projeto
 const contato = collection(database,'contato')
@@ -12,7 +12,7 @@ export default function Read() {
 
   const [contatoLista,setContatoLista] = useState([])
   const read = ()=>{
-    getDocs(contato)
+    getDocs(query(contato, orderBy("nome")))
     .then((data)=>{
       setContatoLista(data.docs.map((item)=>{
         return{...item.data(), id:item.id}
@@ -23,6 +23,16 @@ export default function Read() {
   useEffect(()=>{
     read()
   },[])
+
+  //função do botão excluir
+  const deleteBtn = (id)=>{
+    const documento = doc(database, "contato",id)
+    deleteDoc(documento)
+    .then(()=>{
+      read()
+    }
+    )
+  }
 
   return (
     <>
@@ -40,7 +50,7 @@ export default function Read() {
               <div className='card-footer text-center'>
               <div className="input-group">
               <input type="button" className='btn-outline-warning form-control' value="Alterar" />
-              <input type="button" className='btn-outline-danger form-control' value="Excluir" />  
+              <input type="button" onClick={()=>deleteBtn(lista.id)} className='btn-outline-danger form-control' value="Excluir" />  
               </div>
               </div>
 
