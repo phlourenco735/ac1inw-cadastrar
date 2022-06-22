c38d53f25e2576b112cd68b82d068d11496ad9d8
phlourenco735 <61694156+phlourenco735@users.noreply.github.com>
2022-06-22 10:12:21 -0300

    Add files via upload

--- component/read.jsx
+++ component/read.jsx
@@ -3,7 +3,7 @@ import { useState,useEffect} from 'react'
 
 //importar a config do firebase
 import { app, database } from '../services/firebase'
-import { collection,addDoc, getDocs, orderBy, query, doc, deleteDoc} from 'firebase/firestore';
+import { collection,addDoc, getDocs, orderBy, query, doc, deleteDoc, getDoc} from 'firebase/firestore';
 
 //configurar o Firebase do projeto
 const contato = collection(database,'contato')
@@ -34,8 +34,49 @@ export default function Read() {
     )
   }
 
+  const [ID, setID]=useState(null)
+  const [contatoUnico, setContatoUnico]=useState({})
+  const [mostrar, setMostrar]=useState(false)
+  const [nome, setNome]=useState("")
+  const [email, setEmail]=useState("")
+  const [telefone, setTelefone]=useState("")
+  const [mensagem, setMensagem]=useState("")
+  
+
+  const show = async(id)=>{
+    setID(id)
+    if(ID!=null){
+    const contatoSimples = doc(database,"contato",ID)
+    const resultado = await getDoc(contatoSimples)
+    setContatoUnico({...resultado.data(), id:resultado.id})
+    setNome(contatoUnico.nome)
+    setEmail(contatoUnico.email)
+    setTelefone(contatoUnico.telefone)
+    setMensagem(contatoUnico.mensagem)
+    setMostrar(true)
+    }
+    if(mensagem!=""){
+      setMostrar(true)
+    }
+  }
+  useEffect(()=>{
+    show(ID)
+  },[ID])
+
   return (
     <>
+    {mostrar ?(
+      <div>
+        <h3 className="text-center">ALTERAR</h3>
+        <input type="text" name="nome" placeholder='Nome' className='form-control' id="" required onChange={event=>setNome(event.target.value)} value={nome} />
+        <input type="email" name="email" placeholder='Email' className='form-control' id="" required onChange={event=>setEmail(event.target.value)} value={email} />
+        <input type="tel" name="telefone" placeholder='Telefone' className='form-control' id="" required onChange={event=>setTelefone(event.target.value)} value={telefone} />
+        <textarea name="mensagem" className='form-control' placeholder='Mensagem' id="" onChange={event=>setMensagem(event.target.value)} value={mensagem} ></textarea>
+        <input type="submit" value="SALVAR" className='form-control btn btn-outline-dark' />
+      </div>
+    ):(
+      <></>
+    )}
       
         <h3 className='text-center'>GRAVADOS</h3>
         {contatoLista.map((lista)=>{
@@ -49,7 +90,7 @@ export default function Read() {
               </div>
               <div className='card-footer text-center'>
               <div className="input-group">
-              <input type="button" className='btn-outline-warning form-control' value="Alterar" />
+              <input type="button" className='btn-outline-warning form-control' value="Alterar" onClick={()=>show(lista.id)} />
               <input type="button" onClick={()=>deleteBtn(lista.id)} className='btn-outline-danger form-control' value="Excluir" />  
               </div>
               </div>
